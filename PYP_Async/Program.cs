fe4d3e5458077fa3670bc6b162885c76d30c675c
RasulRustamli <rasular@code.edu.az>
2022-08-30 00:35:32 +0400

    Add project files.

---
+++ PYP_Async/Program.cs
@@ -0,0 +1,8 @@
+﻿using PYP_Async;
+
+List<string> webSite = new List<string> { "https://buttigieg.medium.com/one-year-in-parenting-has-taught-us-about-vulnerability-and-gratitude-170f6e94cbad", "https://medium.com/@anastasiabasilcunningham/crotch-is-not-a-dress-length-797346183e32?source=read_next_recirc---------0---------------------62cb1871_ddf4_413f_9d1f_478bbb5b2045-------", "https://medium.com/@traviswking-ntaa/four-uniquely-mexican-things-that-the-whole-world-should-adopt-12b4799621b", "https://medium.com/the-no%C3%B6sphere?source=read_next_recirc---------0---------------------facf2427_0cee_4865_bc4b_9a433b6813a3-------" };
+
+WebSiteTracker webSiteTracker = new WebSiteTracker();
+await webSiteTracker.Tracker(webSite);
+
+await webSiteTracker.TaskAsync(webSite);
---
+++ PYP_Async/WebSiteTracker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PYP_Async
+{
+    public class WebSiteTracker
+    {
+        public async Task Tracker(List<string> website)
+        {
+            
+            using var client = new HttpClient();
+            var watch = new Stopwatch();
+            var watch2 = new Stopwatch();
+            watch2.Start();
+            foreach (var item in website)
+            {
+                
+                watch.Start();
+
+                var content= await client.GetStringAsync(item);
+                var count = content.Count();
+                watch.Stop();
+                var responseTimeForCompleteRequest = watch.ElapsedMilliseconds;
+                
+                Console.WriteLine($"Character Count = {count} Time = {responseTimeForCompleteRequest}");
+                watch.Reset();
+            }
+            watch2.Stop();
+            Console.WriteLine($"{watch2.ElapsedMilliseconds} ms");
+            Console.WriteLine(new String('_', 70));
+
+
+        }
+        public async Task TaskAsync(List<string> website)
+        {
+            HttpClient client = new HttpClient();
+
+            List<Task<string>> tasks = new List<Task<string>>();
+            Stopwatch watch = new Stopwatch();
+            foreach (string url in website)
+            {
+                tasks.Add(client.GetStringAsync(url));
+            }
+            watch.Start();
+
+            await Task.WhenAll(tasks);
+            watch.Stop();
+            foreach (var task in tasks)
+            {
+                Console.WriteLine($"Character Count = {task.Result.Length} Time = {watch.ElapsedMilliseconds}");
+            }
+            Console.WriteLine(new String('_', 70));
+           
+
+        }
+    }
+}
